37345a5e46c980fdd6c1e5de1bfec623fc25077c
ishutova <i.schutowa2011@yandex.ru>
2022-04-13 20:42:35 +0400

    Добавьте файлы проекта.

---
+++ BinaryTestApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using BinaryTestApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sensorService = new BinarySensorsService(new List<SensorModel>()
+            {
+                new SensorModel(2, (float)1.1235),
+                new SensorModel(5, (float)0.9876543)
+            });
+
+            sensorService.SensorValuesWrite();
+            sensorService.to_csv(); 
+
+            Console.ReadKey();
+        }
+    }
+}
